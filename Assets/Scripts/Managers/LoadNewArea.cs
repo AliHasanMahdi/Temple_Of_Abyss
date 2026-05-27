7ee42930a29032d7748d71c543650cfa08b7b83d
kalateya <khalidalateya1641@gmail.com>
2026-05-27 05:58:26 +0300

    added links
    only need to add level exit and level 1 hud with health

---
+++ Assets/Scripts/Managers/LoadNewArea.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // This gives us access to scene-loading functions
+
+public class LoadNewArea : MonoBehaviour
+{
+    // Type the exact name of the scene you want to load here.
+    // We will add it in the Unity Inspector in the next step.
+    public string sceneToLoad;
+
+    // This function is called automatically when another object's collider enters this trigger zone.
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered is the Player.
+        // This prevents enemies or other objects from accidentally triggering the level change.
+        if (other.CompareTag("Player"))
+        {
+            // Load the scene we specified in the Inspector.
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
